db184fd7a76d479313139828c1266934b8a1ee6a
AbdussalamR <raheem05@tamu.edu>
2026-03-29 23:15:37 -0500

    Changes in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Run jobs inline (synchronously) in development so notifications fire immediately
+  # and any delivery errors are visible rather than silently lost in a background thread.
+  config.active_job.queue_adapter = :inline
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
